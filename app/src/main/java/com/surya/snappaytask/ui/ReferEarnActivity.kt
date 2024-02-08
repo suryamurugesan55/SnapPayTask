9ec8254f968a8a0b442cd2853daf0144667f5c9b
Surya <mssuryamurugesan@gmail.com>
2024-02-08 22:08:26 +0530

    Initial commit

---
+++ app/src/main/java/com/surya/snappaytask/ui/ReferEarnActivity.kt
@@ -0,0 +1,12 @@
+package com.surya.snappaytask.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.surya.snappaytask.R
+
+class ReferEarnActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_refer_earn)
+    }
+}
---
+++ app/src/main/java/com/surya/snappaytask/ui/TrackReferralActivity.kt
@@ -0,0 +1,12 @@
+package com.surya.snappaytask.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.surya.snappaytask.R
+
+class TrackReferralActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_track_referral)
+    }
+}
